ae313dfd2390d06a9ce847c266e3efe89d9918b8
Eric Manning <emanningmusic@yahoo.com>
2019-10-11 21:38:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,4 @@
-speak_to_grandma = "grandma"
-  if speak_to_grandma != "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
-  end
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
