0fac1783efdfc572c057d650cb1a0f62e58f2576
Eric Manning <emanningmusic@yahoo.com>
2019-10-11 21:02:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,13 @@
 speak_to_grandma = "i love you grandma!".upcase
 
 if speak_to_grandma == "i love you grandma!".upcase
-  puts "i love you too pumpkin!".upcase
-  elsif speak_to_grandma == "Hi Nana, how are you?"
-  puts ""
+  puts "I LOVE YOU TOO PUMPKIN!"
+  elsif speak_to_grandma == "hi Nana, how are you?".capitalize
+  puts "HUH?! SPEAK UP, SONNY!"
+  elsif speak_to_grandma == "hi!".capitalize
+  puts "HUH?! SPEAK UP, SONNY!"
+  elsif speak_to_grandma == "what do you want to eat today".upcase
+  puts 
  end 
 
 # Write a speak_to_grandma method.
