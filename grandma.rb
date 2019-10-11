b86487b04d35df1841788c32460057e2bfa2006a
Eric Manning <emanningmusic@yahoo.com>
2019-10-11 22:31:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif string == "Hi Nana, how are you?"
-    return ""
+    return "HUH?! SPEAK UP, SONNY!"
   end
 
 end
